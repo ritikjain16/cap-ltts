37e57fd57dc6456f9caecd40daa11cc3087562b5
Ritik Jain <ritik9628@gmail.com>
2023-12-02 18:13:47 +0530

    commit

--- src/myaxios.jsx
+++ src/myaxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const myaxios = axios.create({
-    // baseURL:"http://localhost:8080"
-    baseURL:"http://65.2.124.51:8080"
-})
+  // baseURL:"http://localhost:8080"
+  baseURL: "http://65.0.83.191:8080",
+});
 
 export default myaxios;
